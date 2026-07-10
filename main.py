179c8b5fa11111ffcf4ec51ea5c7d3250450bbeb
alenanehoda-lab <alenanehoda@icloud.com>
2026-07-10 11:20:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Global Nannies Bot is starting...")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 Привет! Global Nannies Bot работает!")
+
+def main():
+    print("Global Nannies Bot is starting...")
+
+    if not TOKEN:
+        raise ValueError("BOT_TOKEN is not found")
+
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
